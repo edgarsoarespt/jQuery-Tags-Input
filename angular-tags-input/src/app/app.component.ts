479418c6a1ecace9770c60066706fc44ead4a70d
Edgar Soares <edgar.soares.pt@gmail.com>
2020-03-02 22:47:46 +0000

    v1.0.1

--- angular-tags-input/src/app/app.component.ts
+++ angular-tags-input/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tags-input';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    $(function () {
+      $('#tags_2').tagsInput({
+        width: 'auto',
+        onChange: function (elem, elem_tags) {
+          console.log(elem);
+        }
+      });
+    });
+  }
 }
